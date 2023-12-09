e1066c1deabe0f9b4fc66d3ea5c5f47c9ce2879d
SMC242 <56912943+SMC242@users.noreply.github.com>
2023-12-09 15:28:56 +0000

    feat: main function

--- scripts/breakdown-html/app/Main.hs
+++ scripts/breakdown-html/app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
-import Cli (runCli)
+import Cli (Options (optionsInput), runCli)
+import Data.Void (Void)
+import Html (toBreakdownHTML)
+import Markdown (Contents, Format (..), ListElement, formatContents, markdownFormat)
+import Text.Megaparsec (ParseErrorBundle, errorBundlePretty, parse)
+
+parseInput :: Options -> Either (ParseErrorBundle Contents Void) (Format ListElement)
+-- TODO: support input file path
+parseInput opts = parse markdownFormat "STDIN" (optionsInput opts)
 
 main :: IO ()
-main = print =<< runCli
+main = do
+  opts <- runCli
+  case parseInput opts of
+    Left err -> print $ errorBundlePretty err
+    Right format -> print . toBreakdownHTML . formatContents $ format
